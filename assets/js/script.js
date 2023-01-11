e225003ee07f99bfec3d20d47b6c38434ee932e4
DomSpadafora <120040191+DomSpadafora@users.noreply.github.com>
2023-01-10 23:45:54 -0500

    added global variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,17 +3,51 @@
 // 3. Pass the Lat and Long to the GetWeather function
 // 4. Build HTML with the Data we get from the weather
 
-//global variables 
+//global variables for 
 var apiKey = '32bab760dbf7801b6e0e943adadda9044';
 var searchHistory = $('#search-history')
 var citySearch = $('#city-search')
+var clearHistoryBtn = $('#clear-history-btn')
+var currentcity = $('#current-city')
+var currentTemp = $('#current-temp')
+var currentHumidity = $('#current-humidity')
+var currentWindSpeed = $('#current-wind-speed') 
+var uvIndex = $('#uv-index')
+var previousCities = []
+var currentWeather = $('#current-weather')
+//API Key
+var apiKey = '32bab760dbf7801b6e0e943adadda9044';
 
 
-//need to make available recently searched cities
+//need to create the click listener for the search button and go grab the value that was inputted
+$(document).on("click", function(event){
+  event.preventDefault();
+//need to finish...not sure how
+});
+
+
+//need to clear the previous city lists when clear history button is clicked
+clearHistoryBtn.on("click", function(){
+  previousCities = []
+  listArray();
+  $(this).addClass('hide'); 
+});
+
+//This is to Request Open Weather based on city inputted
 
 
 
 
+//Display and save the search history of cities
+
+
+//Create a conditional function to remove the hide class so the hidden text will show once a city is searched
+
+
+
+
+
+///code given to us by our teacher 
 fetch('http://api.openweathermap.org/geo/1.0/direct?appid=2bab760dbf7801b6e0e943adadda9044&q=Indianapolis'
 )
   .then(function (response) {
